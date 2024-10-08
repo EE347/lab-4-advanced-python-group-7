7720c162a72a4c1a88fd60fe6d318c5c4303e7f6
JLuss1 <jacquesluss@gmail.com>
2024-10-08 15:45:19 +0100

    Task 5, write to CSV file using csv package

--- task5.py
+++ task5.py
@@ -1,2 +1,13 @@
 import csv
 
+with open('task5.csv', 'a') as file:
+    # This is a CSV file, but it is not stated in the instructions to add commas after input values, only a new line, so assuming no commas.
+    csvwriter =  csv.writer(file, delimiter='\n')
+    while True:
+        username = input("Give me your name, user: ")
+        if username == "quit":
+            break
+        else:
+            csvwriter.writerow([username])
+
+print("Quitting...")
