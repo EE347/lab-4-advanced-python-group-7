3f3e314131b96d7df30cb31b224ac1623590067d
JLuss1 <jacquesluss@gmail.com>
2024-10-08 15:51:11 +0100

    Fixes to Task 5

--- task5.py
+++ task5.py
@@ -6,8 +6,13 @@ with open('task5.csv', 'a') as file:
     while True:
         username = input("Give me your name, user: ")
         if username == "quit":
+            file.close()
             break
         else:
             csvwriter.writerow([username])
 
-print("Quitting...")
+print("\nPrinting Names:\n")
+
+with open('task5.csv', 'r') as file:    
+    for row in csv.reader(file, delimiter='\n'):
+        print(', '.join(row))
