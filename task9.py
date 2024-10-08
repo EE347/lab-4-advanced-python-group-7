5b9c42dd75eb2105a88aff7dfbe2727d9c13f465
JLuss1 <jacquesluss@gmail.com>
2024-10-08 16:46:05 +0100

    Attempt at Task 9

--- task9.py
+++ task9.py
@@ -1,3 +1,35 @@
 import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
+
+
+plots = {}
+
+with open('task9.csv', 'r') as file:        
+    csvfile = csv.reader(file)
+    headers = next(csvfile)
+
+    data = {}
+
+    for title in headers:
+        print(title)
+        data[title] = []
+
+    for key, value in data.items():
+        print(key)
+
+    for row in csvfile:
+        for index, title in enumerate(headers):
+            data[title].append(row[index])
+
+for i in range(len(data['Date'])):
+    data['Date'][i] = datetime.strptime(data["Date"[i], "%d-%m-%Y"])
+
+for index, (key, value) in enumerate(data.items()):
+    if key == 'Date':
+        continue
+    else:
+        plt.plot(value, data['Date'], label = key)
+
+plt.legend()
+plt.show()
