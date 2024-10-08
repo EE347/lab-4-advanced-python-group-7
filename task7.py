36f236e986b35847111c44d7ee39e2afa8e2033f
JLuss1 <jacquesluss@gmail.com>
2024-10-08 16:00:59 +0100

    Task 7, save to npy file

--- task7.py
+++ task7.py
@@ -3,3 +3,7 @@ import numpy as np
 
 x = np.linspace(0, 10, 101)
 print(x)
+sine_wave = np.sin(x)
+cosine_wave = np.cos(x)
+np.save("task7_sin.npy", sine_wave)
+np.save("task7_cos.npy", cosine_wave)
