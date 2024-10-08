50d319495d98f8d20932828b6224296865604a20
JLuss1 <jacquesluss@gmail.com>
2024-10-08 16:08:12 +0100

    Task 8, plot saved npy values

--- task8.py
+++ task8.py
@@ -1,2 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+cosine = np.load('task7_cos.npy')
+sine = np.load('task7_sin.npy')
+
+plt.plot(cosine, label='Cosine Wave')
+plt.plot(sine, label='Sine Wave')
+plt.legend()
+plt.show()
